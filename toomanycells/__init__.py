0e54338b6c5594c71633deda2d458037d80a4015
JRR3 <javier.ruizramirez@uhn.ca>
2025-02-05 19:33:42 -0500

    Bump version: 1.0.53 → 1.0.54

--- toomanycells/__init__.py
+++ toomanycells/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Javier Ruiz-Ramirez"""
 __email__ = "javier.ruizramirez@uhn.ca"
-__version__ = "1.0.53"
+__version__ = "1.0.54"
 
 from .toomanycells import TooManyCells
 from .common import load_metadata_for_demo
